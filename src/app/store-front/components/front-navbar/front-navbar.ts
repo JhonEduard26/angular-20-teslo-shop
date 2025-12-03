7b9208145fa798840ffa8719567f2c083d7c4835
jhon <jhon.bocanegra26@gmail.com>
2025-12-03 16:15:15 -0500

    feat(store-front): add store front layout and pages with routing
    - Implemented routing for the store front, including Home, Gender, Product, and Not Found pages.
    - Created StoreFrontLayout component to serve as the main layout.
    - Added FrontNavbar component for navigation.
    - Developed individual pages: HomePage, GenderPage, ProductPage, and NotFoundPage.
    - Added corresponding HTML, CSS, and spec files for each component and page.
    - Updated global styles to include Open Props for design consistency.

---
+++ src/app/store-front/components/front-navbar/front-navbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'front-navbar',
+  imports: [],
+  templateUrl: './front-navbar.html',
+  styleUrl: './front-navbar.css',
+})
+export class FrontNavbar {}
