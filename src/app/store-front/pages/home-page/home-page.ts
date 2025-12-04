828e82d2bb76e33392bc2bc7bf692f69deffff37
jhon <jhon.bocanegra26@gmail.com>
2025-12-03 22:54:47 -0500

    feat: implement product card component and enhance store front layout

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductCard } from '@products/components/product-card/product-card';
+import { ProductService } from '@products/services/product-service';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
 export class HomePage {
+  private readonly productService = inject(ProductService);
 
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: ({ params }) => {
+      return this.productService.getProducts({
+        limit: 9,
+        gender: 'men',
+      });
+    },
+  });
 }
